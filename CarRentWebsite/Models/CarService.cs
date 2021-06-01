ab6d08319bdaa606b91615cc025ac2c04c84a732
Ihor Klikun <klikun2001@gmail.com>
2021-06-01 13:28:02 +0300

    Added CarService model

---
+++ CarRentWebsite/Models/CarService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentWebsite.Models
+{
+    public class CarService
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
