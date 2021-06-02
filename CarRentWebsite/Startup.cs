a2675413090d41064e9be97eed0966a88e67d316
Ihor Klikun <klikun2001@gmail.com>
2021-06-02 11:15:26 +0300

    Added swagger

--- CarRentWebsite/Startup.cs
+++ CarRentWebsite/Startup.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
 using CarRentWebsite.Data;
 using CarRentWebsite.Models;
 using Microsoft.AspNetCore.Authentication;
@@ -11,6 +15,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace CarRentWebsite
 {
@@ -49,11 +54,56 @@ namespace CarRentWebsite
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Car Rent API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+                
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                
+                if (!File.Exists(xmlPath))
+                {
+                    using (FileStream fs = File.Create(xmlPath))
+                    {
+                        byte[] info = new UTF8Encoding(true).GetBytes("<?xml version=\"1.0\"?>\r\n<doc></doc>");
+                        fs.Write(info, 0, info.Length);
+                    }
+                }
+                
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
