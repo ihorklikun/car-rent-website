7ca018f2acf2028f939b740eeef286dc4e5cb2d7
Ihor Klikun <klikun2001@gmail.com>
2021-06-14 16:54:04 +0300

    Change Layout Start using Layout in App.js

--- CarRentWebsite/ClientApp/src/App.js
+++ CarRentWebsite/ClientApp/src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { Layout } from './components/Layout';
+import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
+import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
+import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
+import Home from './Pages/HomePage/Home';
+import About from './Pages/AboutUsPage/About';
+import BookingCarPage from './Pages/BookingCarPage/BookingCarPage';
+import CarInfo from './Pages/CarInfoPage/CarInfo';
+import CarManagerInfo from "./Pages/CarManagerInfo/CarManagerInfo";
+import { Layout } from './components/Layout'
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,32 +17,24 @@ import {
   Route
 } from 'react-router-dom';
 
-import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
-import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
-import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
-import NaviBar from './components/Navibar.js'
-import Footer from './components/Footer.js'
-import Home from './Pages/HomePage/Home';
-import About from './Pages/AboutUsPage/About';
-import BookingCarPage from './Pages/BookingCarPage/BookingCarPage';
-import CarInfo from './Pages/CarInfoPage/CarInfo';
-import CarManagerInfo from "./Pages/CarManagerInfo/CarManagerInfo";
 
-function App() {
-  return (
-    <>
-    <Router>
-    <NaviBar/>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path='/carInfo' component={CarInfo} />
-      <Route path='/carManagerInfo' component={CarManagerInfo} />
-      <Route exact path="/about" component={About} />
-      <Route exact path='/bookingCarPage' component={BookingCarPage} />
-      <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
-    </Switch>
-    </Router>
-    <Footer/>
-    </>
-  );
-}export default App;
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <Layout>
+                <Router>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path='/carInfo' component={CarInfo} />
+                    <Route path='/carManagerInfo' component={CarManagerInfo} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path='/bookingCarPage' component={BookingCarPage} />
+                    <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
+                </Router>
+            </Layout>
+        );
+    }
+}
+
+
--- CarRentWebsite/ClientApp/src/components/Layout.js
+++ CarRentWebsite/ClientApp/src/components/Layout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import NavMenu from './Navibar';
+import Footer from './Footer';
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -12,6 +13,7 @@ export class Layout extends Component {
         <Container fluid={true}>
           {this.props.children}
         </Container>
+        <Footer />
       </div>
     );
   }
