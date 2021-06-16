1ed9600c44f6dbd6ea716d854b452a2d1e5300a5
Ihor Klikun <klikun2001@gmail.com>
2021-06-16 16:48:30 +0300

    Create viewModels

---
+++ CarRentWebsite/ViewModels/EngineViewModels/EngineViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentWebsite.ViewModels.EngineViewModels
+{
+    public class EngineViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int FuelCapacity { get; set; }
+        public int Power { get; set; }
+        public int Torque { get; set; }
+        public string WinCode { get; set; }
+    }
+}
