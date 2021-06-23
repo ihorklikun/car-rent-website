4686ed0d6b54a176de3d32c4d4259b1a883cc411
AndriiHarashchak <andriymatiiv@gmail.com>
2021-06-23 18:33:38 +0300

    added updated json. small changes and improvements

--- CarRentWebsite/Startup.cs
+++ CarRentWebsite/Startup.cs
@@ -117,11 +117,11 @@ namespace CarRentWebsite
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
             // specifying the Swagger JSON endpoint.
-            //app.UseSwaggerUI(c =>
-            //{
-            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            //    c.RoutePrefix = string.Empty;
-            //});
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             if (env.IsDevelopment())
             {
