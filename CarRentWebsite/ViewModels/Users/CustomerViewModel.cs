27b89b17df504adfd009e0fd381f64b73129110a
IvanVarvarchuk <vivan2101@gmail.com>
2021-06-24 17:11:23 +0300

    Edit repositories

---
+++ CarRentWebsite/ViewModels/Users/CustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentWebsite.ViewModels.Users
+{
+    public class CustomerViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CustomerInfo { get; set; }
+    }
+}
